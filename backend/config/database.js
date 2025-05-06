ed3e0be0f5cccca1dd1d22afa04da049c0b604f8
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-05-06 16:03:09 +0500

    Final Version

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+import pg from "pg"
 
 const sequelize = new Sequelize("postgresql://InsiderThreat_owner:npg_D9pjQHFLX5oB@ep-twilight-unit-a4d818h9-pooler.us-east-1.aws.neon.tech/InsiderThreat?sslmode=require", {
   dialect: 'postgres',
+dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
